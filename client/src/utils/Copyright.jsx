9bceefd2ce497c7bf2757fc284ed19fdda61c005
yuvraj_singh_rajput <161643356+yuvrajSingh-rajput@users.noreply.github.com>
2024-09-22 22:30:24 +0530

    Update Copyright.jsx

--- client/src/utils/Copyright.jsx
+++ client/src/utils/Copyright.jsx
@@ -5,7 +5,7 @@ function Copyright() {
   return (
     <>
         <div className='text-center text-wrap py-7 bg-gray-300 text-[#4f4d4d] font-semibold'>
-            <h1 className='flex items-center gap-1 justify-center'><span><FaRegCopyright/></span>Copyright - Shiva Singh Rathore</h1>
+            <h1 className='flex items-center gap-1 justify-center'><span><FaRegCopyright/></span>Copyright - Yuvraj Singh Rajput</h1>
             <h1>Date: 20-09-2024</h1>
         </div>
     </>
